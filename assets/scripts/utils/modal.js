3837d8ba437afb2dc894d6b343c94f0f20105a90
GameBak <gamebak@null.co>
2023-10-07 13:38:41 +0200

    CSS Modifications, Hidden overflow when modal is open

--- assets/scripts/utils/modal.js
+++ assets/scripts/utils/modal.js
@@ -2,10 +2,15 @@ export const displayModal = async (name) => {
 	const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 	
+	scrollTo(0, 0);
+	
 	document.getElementById("modal-name").textContent = name;
+	document.body.style.overflow = "hidden";
 }
 
 export const closeModal = () => {
 	const modal = document.getElementById("contact_modal");
+
 	modal.style.display = "none";
+	document.body.style.overflow = "unset";
 }
